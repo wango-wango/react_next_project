08a72ce33738b477da86e0fd108d7fd91003afc5
Jerry Wang <doghost111@gmail.com>
2023-07-22 20:49:03 +0800

    Feat : add resuilts title and error alert

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,15 +1,20 @@
 import React, { Fragment } from "react";
 import { useRouter } from "next/router";
-import { getEventById } from "@/dummy-data";
-import EventSummary from "@/components/event-detail/event-summary";
-import EventLogistics from "@/components/event-detail/event-logistics";
-import EventContent from "@/components/event-detail/event-content";
+import { getEventById } from "src/dummy-data";
+import EventSummary from "src/components/event-detail/event-summary";
+import EventLogistics from "src/components/event-detail/event-logistics";
+import EventContent from "src/components/event-detail/event-content";
+import ErrorAlert from "src/components/events/error-alert";
 function EventDetail() {
   const router = useRouter();
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
   return (
     <Fragment>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { useRouter } from "next/router";
+import { getAllEvents } from "src/dummy-data";
+import EventList from "src/components/events/EventList";
+import EventSearch from "src/components/events/EventSearch";
 function Event() {
+  const router = useRouter();
+  const events = getAllEvents();
+  const onSearch = (year, month) => {
+    console.log(year, month);
+    const fullPath = `/events/${year}/${month}`;
+    // 如果只在 events/ 後面設定一個參數，next 會導入頁面到[eventId]
+    // 如果在 events/ 後面設定多於一個參數，next 會導入頁面到 [...slug]
+    router.push(fullPath);
+  };
   return (
     <div>
-      \<h1>All event</h1>
+      <EventSearch onSearch={onSearch} />
+      <EventList items={events} />
     </div>
   );
 }
